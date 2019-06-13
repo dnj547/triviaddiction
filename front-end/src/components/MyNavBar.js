23c2fff0cafa825a1eb35865c7bf8f87244975ec
dnj547 <dnj547@live.com>
2019-06-13 15:18:29 -0400

    Nav bar

---
+++ front-end/src/components/MyNavBar.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+export default class MyNavBar extends React.Component {
+  render() {
+    return (
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home"><img src="../logo/triviaddiction_logo.png" alt="triviaddiction logo" /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        {this.props.loggedIn ?
+          <Nav className="mr-auto">
+            <Nav.Link href="/signOut">Sign Out</Nav.Link>
+            <Nav.Link href="/myAccount">My Account</Nav.Link>
+          </Nav> : null}
+      </Navbar>
+    )
+  }
+}
