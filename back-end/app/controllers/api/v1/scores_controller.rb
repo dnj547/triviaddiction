a35edc1ea9445aaabadf252f631c79a33101f3ed
Edgar Ong <emailedgar@gmail.com>
2019-06-14 15:57:36 -0400

    sort scoreboard by high score

--- back-end/app/controllers/api/v1/scores_controller.rb
+++ back-end/app/controllers/api/v1/scores_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::ScoresController < ApplicationController
   def index
     # sort highest score by most recent in descending order
-    @scores = Score.all.sort_by {|score| score.created_at}.reverse
-    
+    @scores = Score.all.sort_by {|score| score.score}.reverse
+
     render json: @scores
   end
 
