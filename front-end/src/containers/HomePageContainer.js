cd74eb9fede8f103baffc03aee2307401a159dc1
Edgar Ong <emailedgar@gmail.com>
2019-06-19 09:38:20 -0400

    fixed error message after successful login/signup

--- front-end/src/containers/HomePageContainer.js
+++ front-end/src/containers/HomePageContainer.js
@@ -17,7 +17,7 @@ export default class HomePageContainer extends React.Component {
           <img src="cup-brown.svg" alt="Coffee Cup" className="m-4" height="400px" />
         </div>
         <div className="col-sm-8 mx-auto text-left">
-          <h1 className="p-4 text-center" style={{color: '#FBA9A7'}}>How to Play</h1>
+          <h2 className="p-4 text-center" style={{color: '#FBA9A7'}}>How to Play</h2>
           <h4 className="p-4">1. Choose a time limit and a category</h4>
           <h4 className="p-4">2. Answer as many questions as you can before the coffee runs out</h4>
           <h4 className="p-4">3. Play it again and again and again and again and again and again and again... until you’re <strong><u>triviaddicted</u></strong> :)</h4>
